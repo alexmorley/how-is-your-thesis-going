b01b68bd917a7e68bea92eda94d879c5f752e174
alexmorley <alexander.morley@univ.ox.ac.uk>
2018-11-24 09:55:53 +0000

    fix trailing commas

--- get_word_count.sh
+++ get_word_count.sh
@@ -1,7 +1,8 @@
 THESIS_LOCATION="../thesis_latex/text"
-echo -n "`date +%Y-%m-%d-%H-%M`, "
+echo -n "`date +%Y-%m-%d-%H-%M`,"
 for i in `seq 1 7`; 
 do 
   x=`wc -w $THESIS_LOCATION/ch$i* | awk '{print $1}'`
-  echo -n "$x, "
+  echo -n "$x,"
 done
+echo
--- update_word_count.sh
+++ update_word_count.sh
@@ -1,1 +1,2 @@
-bash get_word_count.sh >> data.csv
+bash get_word_count.sh >> data_raw.csv
+sed -s 's/,*\r*$//' data_raw.csv > data.csv
