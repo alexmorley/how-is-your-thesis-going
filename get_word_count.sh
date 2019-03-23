90bd745d9bdd5ff64523688ed34cbc06e11c21ad
Alexander Morley <alexander.morley@univ.ox.ac.uk>
2019-03-23 21:24:35 +0000

    add figures line

--- get_word_count.sh
+++ get_word_count.sh
@@ -1,8 +1,10 @@
-THESIS_LOCATION="../thesis_latex/text"
+THESIS_LOCATION="../thesis_latex"
 echo -n "`date +%Y-%m-%d-%H-%M`,"
 for i in `seq 1 7`; 
 do 
-  x=`wc -w $THESIS_LOCATION/ch$i* | awk '{print $1}'`
+  x=`wc -w $THESIS_LOCATION/text/ch$i* | awk '{print $1}'`
   echo -n "$x,"
 done
+f=`ls $THESIS_LOCATION/figures/*png | wc -l`
+echo -n "$f"
 echo
